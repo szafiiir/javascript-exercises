c4f7b05a67b4fae1ed84df0c11436ace24be6b6b
szafiiir <maniek2002388@gmail.com>
2025-12-18 21:44:27 +0100

    pass the third test, returns false when number is 9

--- foundations/03_numberChecker/numberChecker.spec.js
+++ foundations/03_numberChecker/numberChecker.spec.js
@@ -4,10 +4,10 @@ describe('numberChecker', () => {
   test.skip('returns true when number is 1000', () => {
     expect(numberChecker(1000)).toEqual(true);
   });
-  test('returns true when number is 10', () => {
+  test.skip('returns true when number is 10', () => {
     expect(numberChecker(10)).toEqual(true);
   });
-  test.skip('returns false when number is 9', () => {
+  test('returns false when number is 9', () => {
     expect(numberChecker(9)).toEqual(false);
   });
   test.skip('returns false when number is 6', () => {
