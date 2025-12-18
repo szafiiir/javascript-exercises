c2806b77a2d123e6060ad1cda77dae2649c5006a
szafiiir <maniek2002388@gmail.com>
2025-12-19 00:59:20 +0100

    first task finished, repeat string hey three times

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(textString, count) {
 
+    if (count < 0) return 'error';
+    
+    let str = '';
+
+    for (let i = 0; i < count; i++) {
+        str += textString;
+    }
+    
+    return str;
 };
+
+repeatString('hey', 3);
+
 
 // Do not edit below this line
 module.exports = repeatString;
