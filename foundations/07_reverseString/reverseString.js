bd9942e99bb9ea7cdaab80d3dbbbf4e695d702ed
szafiiir <maniek2002388@gmail.com>
2025-12-22 01:34:19 +0100

    finish 07_reverseString task

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,19 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    
+    let revStr = '';
+    
+    for (let i = str.length - 1; i >= 0; i--) {
+        revStr += str[i];
+    }
+    
+    return revStr;
 
 };
+
+reverseString('hello');
+reverseString('hello there');
+reverseString('123! abc! Hello, Odinite.')
+reverseString('');
 
 // Do not edit below this line
 module.exports = reverseString;
