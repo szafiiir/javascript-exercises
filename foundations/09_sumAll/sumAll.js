b10124851dd0b18d03e7a17d5c82b90dd212c95f
szafiiir <maniek2002388@gmail.com>
2025-12-24 01:18:20 +0100

    finished task 09sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,14 +1,29 @@
-const sumAll = function(a, b) {
+const sumAll = function (a, b) {
 
-    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b) || typeof a !== 'number' || typeof b !== 'number') {
-        return 'ERROR';
+  if (
+    a < 0 ||
+    b < 0 ||
+    !Number.isInteger(a) ||
+    !Number.isInteger(b) ||
+    typeof a !== "number" ||
+    typeof b !== "number"
+  ) {
+    return "ERROR";
+  } else if (a <= b) {
+
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
     }
-    else if (a <= b) {
-        
+    return sum;
+  } else if (a > b) {
 
+    let sum = 0;
+    for (let i = b; i <= a; i++) {
+        sum += i;
     }
-
-
+    return sum;
+  }
 };
 
 // Do not edit below this line
