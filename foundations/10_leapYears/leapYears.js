4ed1d89f2e2f7611e4c2abbf2f917f234989f2b4
szafiiir <maniek2002388@gmail.com>
2025-12-24 02:16:35 +0100

    finish task 10leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const fourDiv = year % 4 === 0;
+    const hundredDiv = year % 100 === 0;
+    const fourHundredDiv = year % 400 === 0;
 
+    if (fourDiv && (!hundredDiv || fourHundredDiv)) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
