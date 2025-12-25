0c3dfd21c141f947559522dcd01bd8223e55de84
szafiiir <maniek2002388@gmail.com>
2025-12-25 02:23:36 +0100

    finished task 11tempConversion, but i used a different path to get there. Solution uses math.round and i can see why. My code got there using toPrecision and while it works for these examples, overall it will be a problem when somebody wants to use a bigger number. Lesson learned.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fDegrees) {
+  let celcius = (fDegrees - 32) * 5 / 9;
+  if (!Number.isInteger(celcius)) {
+    return +celcius.toPrecision(3);
+  }
+  else {
+    return celcius;
+  }
 };
 
-const convertToFahrenheit = function() {
+
+const convertToFahrenheit = function(cDegrees) {
+
+  let fahrenheit = (cDegrees * 9 / 5 + 32);
+  if (!Number.isInteger(fahrenheit)) {
+    return +fahrenheit.toPrecision(4);
+  }
+  else {
+    return fahrenheit;
+  }
+
+
 };
 
 // Do not edit below this line
