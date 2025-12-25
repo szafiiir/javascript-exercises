ecf2a2b35bb96a8bf65cd67ab234b266a4fd134c
szafiiir <maniek2002388@gmail.com>
2025-12-26 00:07:46 +0100

    finish task 12calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -17,16 +17,25 @@ const sum = function(array) {
   return sumWithInitial;
 };
 
-const multiply = function() {
-  
+const multiply = function(array) {
+  let multiplySum = 1;
+  for (let i = 0; i < array.length; i++) {
+    multiplySum *= array[i];
+  }
+  return multiplySum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let powerSum = a ** b;
+  return powerSum;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+    let factorialSum = 1;
+    for (let i = 1; i <= n; i++) {
+        factorialSum *= i;
+    }
+    return factorialSum;
 };
 
 // Do not edit below this line
