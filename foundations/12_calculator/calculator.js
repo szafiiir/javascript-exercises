3ae2655772a971195eced12d4f178c403b70fddd
szafiiir <maniek2002388@gmail.com>
2025-12-25 07:26:07 +0100

    half finish task 12calculator, multiplying arrays is next and i know how to do that, tommorow ill come back!

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,17 +1,24 @@
-const add = function() {
-	
+const add = function(a, b) {
+	let sum = a + b;
+  return sum;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	let sum = a - b;
+  return sum;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const initialValue = 0;
+  const sumWithInitial = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue,
+    initialValue,
+  );
+  return sumWithInitial;
 };
 
 const multiply = function() {
-
+  
 };
 
 const power = function() {
