7f67fe57d1c43b5ac9624992735a789736df3204
szafiiir <maniek2002388@gmail.com>
2025-12-31 20:18:52 +0100

    finish task 13_palindromes oh yesssss

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,4 +1,17 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+    let textArr = Array.from(text);
+
+    let noPunctLowCase_textArr = textArr.map(letter => letter.replace(/[."!? ,]/g, "").toLowerCase());
+    let noPunctLowCase_textArrReversed = noPunctLowCase_textArr.toReversed();
+
+    if (noPunctLowCase_textArr.join("") === noPunctLowCase_textArrReversed.join("")) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
 
 };
 
