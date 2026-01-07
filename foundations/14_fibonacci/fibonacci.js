d547500afa4d41ff9a8e8cec9df9263a9ab1177f
szafiiir <maniek2002388@gmail.com>
2026-01-07 21:09:50 +0100

    finish task 14fibonacci, used the iterative approach but i started my array with 1, 1

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
 
+    if (member == 0) return 0;
+    if (member < 0) return "OOPS";
+
+    let fib = [1, 1];
+    let sum = 0;
+
+    for (let i = 0; i < member; i++) {
+        sum = fib[i] + fib[i + 1];
+        fib.push(sum);
+    }
+
+    return fib[member - 1];
 };
 
 // Do not edit below this line
